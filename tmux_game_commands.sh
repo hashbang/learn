40e16dce74f68d3c30d257688e75f80176fa792e
Daniel Lewis <dlewis@getpebble.com>
2014-10-07 00:11:47 -0700

    WHAT. LIKE YOU COULD DO BETTER.

---
+++ tmux_game_commands.sh
@@ -0,0 +1,18 @@
+
+function get_active_window() {
+  tmux list-windows | grep "\* ([0-9]* panes)" | sed 's/:.*//'
+}
+function get_active_pane() {
+  tmux select-window -t `get_active_window`
+  tmux list-panes | grep "(active)$" | sed 's/:.*//'
+}
+
+# add_notice "
+# Hello HashBanger!
+# We would like to
+# invite you to a 
+# celebration!"
+function add_notice() {
+  tmux split-window -t `get_active_pane` -v -p 50 "echo \"$@\"; read"
+}
+
